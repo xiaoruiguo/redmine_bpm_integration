d4f68652f263dce028588b09fe95495c6b301f62
Thales Pires <thalestpires@gmail.com>
2015-09-10 00:42:37 -0300

    habilitando autenticação no bpm

--- app/models/httparty.rb
+++ app/models/httparty.rb
@@ -3,9 +3,14 @@ class Httparty
   format :json
   base_uri Setting.plugin_bpm_integration[:bpms_url]
 
+  @@auth = {
+    username: Setting.plugin_bpm_integration[:bpms_user],
+    password: Setting.plugin_bpm_integration[:bpms_pass]
+  }
+
   def process_list
     processes = []
-    self.class.get('/repository/process-definitions')["data"].each do |p|
+    self.class.get('/repository/process-definitions', basic_auth: @@auth)["data"].each do |p|
       processes << BpmProcess.new( p )
     end
     return processes
@@ -14,6 +19,7 @@ class Httparty
   def start_process(process_key, form)
     self.class.post(
       '/runtime/process-instances',
+      basic_auth: @@auth,
       body: start_process_request_body(process_key, form),
       headers: { 'Content-Type' => 'application/json' }
     )
@@ -22,6 +28,7 @@ class Httparty
   def getFormData(processId)
     self.class.get(
       '/form/form-data',
+      basic_auth: @@auth,
       query: { processDefinitionId: processId },
       headers: { 'Content-Type' => 'application/json' }
     )
@@ -37,7 +44,7 @@ class Httparty
   end
 
   def bpm_tasks
-    hash_bpm_taks = self.class.get('/runtime/tasks')
+    hash_bpm_taks = self.class.get('/runtime/tasks', basic_auth: @@auth)
     tasks = []
     hash_bpm_taks["data"].each do |t|
       tasks << BpmTask.new(t)
--- init.rb
+++ init.rb
@@ -1,10 +1,9 @@
 Redmine::Plugin.register :bpm_integration do
   name 'BPM Integration Plugin'
   author 'Filipe Xavier, Lucas Arnaud, Thales Pires'
-  description 'This is a plugin for integrating Redmine with any BPMS like Activiti and Bonita.'
+  description 'This is a plugin for integrating Redmine with Activiti BPM.'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/thalestpires/redmine_bpm_integration'
 
   menu :top_menu  , :bpm_processes, { controller: 'bpm_processes', action: 'index' } , caption: :bpm_processes
 

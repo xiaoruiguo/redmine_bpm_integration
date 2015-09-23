3b30eb882fcf478d33ac33fa77b2261b8a38dde8
filipe.xavier <filipe.xavier@visagio.com>
2015-09-23 01:00:48 -0300

    Fix null pointer in process update on controller

--- app/controllers/process_definitions_controller.rb
+++ app/controllers/process_definitions_controller.rb
@@ -11,12 +11,18 @@ class ProcessDefinitionsController < BpmController
 
   def edit
     @process_definition = BpmIntegration::ProcessDefinition.find(params[:id])
+    @process_definition.tracker_process_definition = BpmIntegration::TrackerProcessDefinition.find(@process_definition.tracker_process_definition_id) unless @process_definition.tracker_process_definition_id.blank?
+    @process_definition
   end
 
   def update
     @process_definition = BpmIntegration::ProcessDefinition.find(params.require(:id))
+    if @process_definition.tracker_process_definition_id.blank?
+      @process_definition.tracker_process_definition ||= BpmIntegration::TrackerProcessDefinition.new
+      @process_definition.tracker_process_definition.process_definition_key ||= @process_definition.key
+    end
     @process_definition.tracker_process_definition.update_attributes(
-        params.require(:bpm_integration_process_definition).require(:tracker_process_definition).permit(:tracker_id)
+      params.require(:bpm_integration_process_definition).require(:tracker_process_definition).permit(:tracker_id)
     )
     # @process_definition.save!
     # @process_definition.tracker_process_definition.save!
@@ -31,6 +37,7 @@ class ProcessDefinitionsController < BpmController
       response = BpmProcessDefinitionService.deploy_process(process_data)
       if !response.blank? && response.code == 201
         handle_sucess('msg_process_uploaded')
+        SynchronizeProcessDefinitionsJob.perform_now
       else
         handle_error('msg_process_upload_error')
       end

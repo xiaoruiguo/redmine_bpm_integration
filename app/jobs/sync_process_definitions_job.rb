d23b0cb9a396fe05c3a476591cacd2a34116144b
Lucas Arnaud <lucas.arnaud92@gmail.com>
2015-10-04 17:48:53 -0300

    Refactoring SyncProcessDefinitionsJob - extracting methods

--- app/jobs/sync_process_definitions_job.rb
+++ app/jobs/sync_process_definitions_job.rb
@@ -10,15 +10,9 @@ class SyncProcessDefinitionsJob < ActiveJob::Base
   def synchronize
     BpmProcessDefinitionService.process_list.each do |process|
       next if BpmIntegration::ProcessDefinition.where(process_identifier:process.id).first
-      new_process = BpmIntegration::ProcessDefinition.new
-      new_process.process_identifier = process.id
-      new_process.description = process.description
-      new_process.name = process.name
-      new_process.key = process.key
-      new_process.version = process.version
 
+      new_process = build_process_definition(process)
       new_process.form_fields = synchronize_form_fields(new_process)
-
       new_process.task_definitions = synchronize_task_definitions(new_process)
 
       if new_process.save(validate:false)
@@ -34,18 +28,9 @@ class SyncProcessDefinitionsJob < ActiveJob::Base
 
     form_fields = []
     form_properties.each do |form_item|
-      field_definition = BpmIntegration::FormFieldDefinition.process_field(process.process_identifier, form_item["id"])
-                          .first_or_create do |ffd|
-        ffd.process_definition = process
-        ffd.name = form_item["name"]
-        ffd.field_type = form_item["type"]
-      end
+      field_definition = find_or_create_form_field_definition(process, form_item)
 
-      form_field = BpmIntegration::FormField.new
-      form_field.readable = form_item["readable"]
-      form_field.writable = form_item["writable"]
-      form_field.required = form_item["required"]
-      form_field.date_pattern = form_item["datePattern"]
+      form_field = build_form_field_from_hash(form_item)
 
       form_field.form_field_definition = field_definition
       form_field.form_able = process
@@ -65,22 +50,11 @@ class SyncProcessDefinitionsJob < ActiveJob::Base
 
       task_form_properties = t["taskFormHandler"]["formPropertyHandlers"]
 
-
       form_fields = []
       task_form_properties.each do |form_item|
-        field_definition = BpmIntegration::FormFieldDefinition.process_field(process.process_identifier, form_item["id"])
-                            .first_or_create do |ffd|
-          ffd.process_definition = process
-          ffd.name = form_item["name"]
-          ffd.field_type = form_item["type"]
-        end
+        field_definition = find_or_create_form_field_definition(process, form_item)
 
-        form_field = BpmIntegration::FormField.new
-        form_field.readable = form_item["readable"]
-        form_field.writable = form_item["writable"]
-        form_field.required = form_item["required"]
-        form_field.date_pattern = form_item["datePattern"]
-
+        form_field = build_form_field_from_hash(form_item)
         form_field.form_field_definition = field_definition
 
         form_fields << form_field
@@ -91,6 +65,38 @@ class SyncProcessDefinitionsJob < ActiveJob::Base
     end
 
     tasks
+  end
+
+  private
+
+  def build_process_definition(bpm_process_definition)
+    process = BpmIntegration::ProcessDefinition.new
+    process.process_identifier = bpm_process_definition.id
+    process.description = bpm_process_definition.description
+    process.name = bpm_process_definition.name
+    process.key = bpm_process_definition.key
+    process.version = bpm_process_definition.version
+
+    process
+  end
+
+  def find_or_create_form_field_definition(process, form_item_hash)
+    BpmIntegration::FormFieldDefinition.process_field(process.process_identifier, form_item_hash["id"])
+          .first_or_create do |ffd|
+      ffd.process_definition = process
+      ffd.name = form_item_hash["name"]
+      ffd.field_type = form_item_hash["type"]
+    end
+  end
+
+  def build_form_field_from_hash(form_item_hash)
+    form_field = BpmIntegration::FormField.new
+    form_field.readable = form_item_hash["readable"]
+    form_field.writable = form_item_hash["writable"]
+    form_field.required = form_item_hash["required"]
+    form_field.date_pattern = form_item_hash["datePattern"]
+
+    form_field
   end
 
 end

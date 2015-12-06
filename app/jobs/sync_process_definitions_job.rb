dd026785d53a2138ef9e97bf6c11cdc7154d097f
Lucas Arnaud <lucas.arnaud@visagio.com>
2015-12-06 01:16:07 -0200

    Fixing single process definition synchronization. It was calling the BPMS WebService with a wrong parameter that did not return the deployed process definition

--- app/jobs/sync_process_definitions_job.rb
+++ app/jobs/sync_process_definitions_job.rb
@@ -28,12 +28,12 @@ class SyncProcessDefinitionsJob < ActiveJob::Base
     e.backtrace.each { |line| Delayed::Worker.logger.error line }
   end
 
-  def synchronize_single_process(process_definition_id)
-    Delayed::Worker.logger.info "#{self.class} - Sincronizando apenas process_definition " + process.id.to_s
-    process = BpmProcessDefinitionService.process_definition(process_definition_id)
+  def synchronize_single_process(deployment_id)
+    Delayed::Worker.logger.info "#{self.class} - Sincronizando apenas process_definition - deploymentId: #{deployment_id}"
+    process = BpmProcessDefinitionService.process_definition_by_deployment_id(deployment_id)
     save_process_definition(process)
   rescue => e
-    Delayed::Worker.logger.error "#{self.class} - Ocorreu um erro ao realizar cadastro do processo " + process_definition_id.to_s
+    Delayed::Worker.logger.error "#{self.class} - Ocorreu um erro ao realizar cadastro do processo - deploymentId: #{deployment_id}"
     e.backtrace.each { |line| Delayed::Worker.logger.error line }
   end
 
--- app/services/bpm_process_definition_service.rb
+++ app/services/bpm_process_definition_service.rb
@@ -10,6 +10,16 @@ class BpmProcessDefinitionService < ActivitiBpmService
     BpmProcessDefinition.new(process)
   end
 
+  def self.process_definition_by_deployment_id(deployment_id)
+    process = get(
+      "/repository/process-definitions",
+      query: { deploymentId: deployment_id },
+      basic_auth: @@auth
+    )["data"].first
+
+    BpmProcessDefinition.new(process)
+  end
+
   def self.process_list
     processes = []
     process_list = get(

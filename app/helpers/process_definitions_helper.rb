ad1ddf6545c8a3c0f7c0e4706cdd6a34483f3b1a
filipexavier <filipexts@gmail.com>
2015-12-09 00:53:11 -0200

    fixes #38882 Permite configurar usuários, grupos e projetos como constantes no processo

--- app/helpers/process_definitions_helper.rb
+++ app/helpers/process_definitions_helper.rb
@@ -11,6 +11,27 @@ module ProcessDefinitionsHelper
           ),
           include_blank: true
         )
+    when 'user_id'
+      select_tag("#{form.object_name}[#{constant.id}]", options_from_collection_for_select(
+                                      User.all, :id, :firstname,
+                                      constant.value.to_i
+          ),
+          include_blank: true
+        )
+    when 'group_id'
+      select_tag("#{form.object_name}[#{constant.id}]", options_from_collection_for_select(
+                                      Group.all, :id, :lastname,
+                                      constant.value.to_i
+          ),
+          include_blank: true
+        )
+    when 'project_id'
+      select_tag("#{form.object_name}[#{constant.id}]", options_from_collection_for_select(
+                                      Project.all, :id, :name,
+                                      constant.value.to_i
+          ),
+          include_blank: true
+        )
     else
       form.text_field :value, name: "#{form.object_name}[#{constant.id}]"
     end

772d914705cb9e543784985be1ef8dd84f63a516
Lucas Arnaud <lucas.arnaud@visagio.com>
2015-12-10 20:20:40 -0200

    destroy dependent process instance

--- app/models/bpm_integration/process_definition_version.rb
+++ app/models/bpm_integration/process_definition_version.rb
@@ -6,7 +6,7 @@ class BpmIntegration::ProcessDefinitionVersion < BpmIntegrationBaseModel
   has_many :task_definitions
   has_many :form_fields, class_name: 'FormField', as: :form_able
   has_many :form_field_definitions, autosave: true, dependent: :destroy
-  has_many :issue_process_instances
+  has_many :issue_process_instances, dependent: :destroy
   has_many :constants, autosave: true, dependent: :destroy, class_name: 'ProcessConstant'
   has_many :end_events, autosave: true, dependent: :destroy, class_name: 'ProcessEndEvent'
 

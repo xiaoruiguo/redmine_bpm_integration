a0c894c205f655cdac7e0d4fd08a342a7179d8c7
filipexavier <filipexts@gmail.com>
2015-12-13 19:18:05 -0200

    Conserta job, e determina tempo de regularidade dos jobs

--- lib/bpm_integration/utils/sync_jobs_period.rb
+++ lib/bpm_integration/utils/sync_jobs_period.rb
@@ -1,7 +1,7 @@
 class SyncJobsPeriod
 
-  @bpm_task = 20.seconds
-  @process_instance = 20.seconds
+  @bpm_task = 60.seconds
+  @process_instance = 60.seconds
 
   def self.bpm_task_period
     @bpm_task
--- run_jobs.sh
+++ run_jobs.sh
@@ -1,4 +1,4 @@
-bin/delayed_job --pool=bpm_tasks:1 --pool=bpm_process_instances:1 start
+RAILS_ENV=$1 bin/delayed_job --pool=bpm_tasks:1 --pool=bpm_process_instances:1 start
 
-rake redmine:plugins:bpm_integration:sync_bpm_tasks
-rake redmine:plugins:bpm_integration:sync_process_instances
+RAILS_ENV=$1 rake redmine:plugins:bpm_integration:sync_bpm_tasks
+RAILS_ENV=$1 rake redmine:plugins:bpm_integration:sync_process_instances

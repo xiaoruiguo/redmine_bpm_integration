6444d34bb313765c8a442997ac912a3d91c4f96c
Lucas Arnaud <lucas.arnaud@visagio.com>
2016-01-25 17:21:53 -0200

    Run start_process_job to retry to start process that had errors on creation

--- run_jobs.sh
+++ run_jobs.sh
@@ -2,3 +2,4 @@ RAILS_ENV=$1 bin/delayed_job --pool=bpm_tasks:1 --pool=bpm_process_instances:1 s
 
 RAILS_ENV=$1 rake redmine:plugins:bpm_integration:sync_bpm_tasks
 RAILS_ENV=$1 rake redmine:plugins:bpm_integration:sync_process_instances
+RAILS_ENV=$1 rake redmine:plugins:bpm_integration:start_process
